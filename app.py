f66c7a8552f38c1298aa349d8c003ac6ec137416
alvinmoore703-ui <alvinmoore703@gmail.com>
2025-12-20 06:16:25 +0100

    Configure app to run on specified PORT environment variable

--- app.py
+++ app.py
@@ -207,5 +207,8 @@ def statement():
     return send_file(file, as_attachment=True)
 
 # ---------------- RUN ----------------
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
