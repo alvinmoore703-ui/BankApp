14ab7f8dbf5a029d5695bb5b2eb9c57b00067e00
alvinmoore703-ui <alvinmoore703@gmail.com>
2025-12-25 18:01:43 +0100

    Update app.py
    Signed-off-by: alvinmoore703-ui <alvinmoore703@gmail.com>

--- app.py
+++ app.py
@@ -25,6 +25,13 @@ mail = Mail(app)
 @app.route("/")
 def home():
     return render_template("index.html")
+    
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        return redirect("/")
+
+    return render_template("register.html")
 
 @app.route("/login")
 def login():
