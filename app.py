86b6e27e87c4b1a8fd03a5811c3097ded4882d0b
alvinmoore703-ui <alvinmoore703@gmail.com>
2025-12-25 16:30:12 +0100

    Update app.py
    Signed-off-by: alvinmoore703-ui <alvinmoore703@gmail.com>

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ def home():
 @app.route("/login")
 def login():
     return "Login page"
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
